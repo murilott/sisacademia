da3c6686a137c8c477a4165461f45d420ce0f11f
MURILO THOMASELLI TEICHERT <murilo.teichert@univille.br>
2022-11-22 14:13:39 -0300

    homecontroller com usuário, dieta e rotina

--- src/main/java/br/univille/sisacademia/controller/HomeController.java
+++ src/main/java/br/univille/sisacademia/controller/HomeController.java
@@ -1,10 +1,14 @@
 package br.univille.sisacademia.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.sisacademia.service.DietaService;
+import br.univille.sisacademia.service.RotinaService;
 import br.univille.sisacademia.service.UsuarioService;
 
 @Controller
@@ -13,11 +17,21 @@ public class HomeController {
 
     private UsuarioService service;
 
+    private RotinaService rotinaService;
+
+    private DietaService dietaService;
+
     @GetMapping
     public ModelAndView index(){
         var listaUsuarios = service.getAll();
+        var listaDietas = dietaService.getAll();
+        var listaRotinas = rotinaService.getAll();
+        HashMap<String, Object> dados = new HashMap<>();
+        dados.put("listaDietas", listaDietas);
+        dados.put("listaRotinas", listaRotinas);
+        dados.put("listaUsuarios", listaUsuarios);
 
-        return new ModelAndView("home/index", "listaUsuarios", listaUsuarios);
+        return new ModelAndView("home/index", dados);
     }
 
     
